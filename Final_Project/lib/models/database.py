e3cf3f5f5e2847b57070cd5b841510810b8aebee
George <mukoshig@gmail.com>
2025-10-16 20:05:45 +0300

    additional of assets:saftey boots and overall

--- Final_Project/lib/models/database.py
+++ Final_Project/lib/models/database.py
@@ -1,27 +1,22 @@
 import sqlite3
 import os
 
-# Bulding an absolute path to the database file to ensure it is correctly located
-# regardless of the current working directory.
+# --- Database setup ---
 DB_PATH = os.path.join(
     os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
     "db",
     "database.db"
 )
-
-# Ensure the db/ folder exists before closing the database connection.
 os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
 
-# Establishing a connection to the SQLite database
-# and creating a cursor for executing SQL commands.
 CONN = sqlite3.connect(DB_PATH)
 CURSOR = CONN.cursor()
 
+
 def create_tables():
-    """Create all required tables."""
+    """Create tables for employees and assets."""
     CURSOR.execute("PRAGMA foreign_keys = ON;")
 
-    # Employee Table
     CURSOR.execute("""
         CREATE TABLE IF NOT EXISTS employees (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -30,12 +25,13 @@ def create_tables():
         );
     """)
 
-    # Asset Table
     CURSOR.execute("""
         CREATE TABLE IF NOT EXISTS assets (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             asset_type TEXT NOT NULL,
-            serial_number TEXT NOT NULL UNIQUE,
+            serial_number TEXT UNIQUE,
+            boot_size TEXT,
+            overall_size TEXT,
             employee_id INTEGER,
             notes TEXT,
             FOREIGN KEY (employee_id) REFERENCES employees(id) ON DELETE SET NULL
@@ -43,4 +39,3 @@ def create_tables():
     """)
 
     CONN.commit()
-    
